bd0678bd9980560a04a8c294883d14ca2e377a61
Kirepreshanth Murugadas <kirepreshanth@outlook.com>
2019-11-10 19:18:54 +0000

    GMask R-CNN script has been created.

---
+++ generator.py
@@ -0,0 +1,121 @@
+import os
+import random
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+from model.networks import Generator
+from utils.tools import get_config, random_bbox, mask_image, is_image_file, default_loader, normalize, get_model_list
+from PIL import Image
+
+
+def generateInpaintedImage(args, imagePath):
+    config = get_config(args.config)
+
+    # CUDA configuration
+    cuda = config['cuda']
+    device_ids = config['gpu_ids']
+    if cuda:
+        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(i) for i in device_ids)
+        device_ids = list(range(len(device_ids)))
+        config['gpu_ids'] = device_ids
+        cudnn.benchmark = True
+
+    print("Arguments: {}".format(args))
+
+    # Set random seed
+    if args.seed is None:
+        args.seed = random.randint(1, 10000)
+    print("Random seed: {}".format(args.seed))
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if cuda:
+        torch.cuda.manual_seed_all(args.seed)
+
+    print("Configuration: {}".format(config))
+
+    try:  # for unexpected error logging
+        with torch.no_grad():   # enter no grad context
+            if is_image_file(imagePath):
+                if args.mask and is_image_file(args.mask):
+                    # Test a single masked image with a given mask
+                    x = default_loader(imagePath)
+                    mask = default_loader(args.mask)
+                    #x = transforms.Resize(config['image_shape'][:-1])(x)
+                    x = transforms.CenterCrop(config['image_shape'][:-1])(x)
+                    mask = transforms.Resize(config['image_shape'][:-1])(mask)
+                    mask = transforms.CenterCrop(config['image_shape'][:-1])(mask)
+                    x = transforms.ToTensor()(x)
+                    mask = transforms.ToTensor()(mask)[0].unsqueeze(dim=0)
+                    x = normalize(x)
+                    x = x * (1. - mask)
+                    x = x.unsqueeze(dim=0)
+                    mask = mask.unsqueeze(dim=0)
+                elif args.mask:
+                    raise TypeError("{} is not an image file.".format(args.mask))
+                else:
+                    # Test a single ground-truth image with a random mask
+                    ground_truth = default_loader(imagePath)
+                    #ground_truth = transforms.Resize(config['image_shape'][:-1])(ground_truth)
+                    ground_truth = transforms.CenterCrop(config['image_shape'][:-1])(ground_truth)
+                    ground_truth = transforms.ToTensor()(ground_truth)
+                    ground_truth = normalize(ground_truth)
+                    ground_truth = ground_truth.unsqueeze(dim=0)
+                    bboxes = random_bbox(config, batch_size=ground_truth.size(0))
+                    x, mask = mask_image(ground_truth, bboxes, config)
+
+                # Set checkpoint path
+                if not args.checkpoint_path:
+                    checkpoint_path = os.path.join('checkpoints',
+                                                   config['dataset_name'],
+                                                   config['mask_type'] + '_' + config['expname'])
+                else:
+                    checkpoint_path = args.checkpoint_path
+
+                # Define the trainer
+                netG = Generator(config['netG'], cuda, device_ids).cuda()
+                # Resume weight
+                last_model_name = get_model_list(checkpoint_path, "gen", iteration=args.iter)
+                netG.load_state_dict(torch.load(last_model_name))
+                model_iteration = int(last_model_name[-11:-3])
+                print("Resume from {} at iteration {}".format(checkpoint_path, model_iteration))
+
+                if cuda:
+                    netG = nn.parallel.DataParallel(netG, device_ids=device_ids)
+                    x = x.cuda()
+                    mask = mask.cuda()
+
+                # Inference
+                x1, x2, offset_flow = netG(x, mask)
+                inpainted_result = x2 * mask + x * (1. - mask)
+
+                default_image = default_loader(imagePath)
+                di_w, di_h = default_image.size
+                inp_hw = config['image_shape'][1]
+                offset = ((di_w - inp_hw) // 2, (di_h - inp_hw) // 2)               
+                
+                grid = vutils.make_grid(inpainted_result, normalize=True)                    
+                    
+                # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
+                ndarr = grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+                im = Image.fromarray(ndarr)
+                
+                default_image.paste(im, offset)
+                
+                return default_image
+                
+                #default_image.save(args.output)
+                
+                print("Saved the inpainted result to {}".format(args.output))
+                if args.flow:
+                    vutils.save_image(offset_flow, args.flow, padding=0, normalize=True)
+                    print("Saved offset flow to {}".format(args.flow))
+            else:
+                raise TypeError("{} is not an image file.".format)
+        # exit no grad context
+    except Exception as e:  # for unexpected error logging
+        print("Error: {}".format(e))
+        raise e
--- run.py
+++ run.py
@@ -1,37 +1,26 @@
+from argparse import ArgumentParser
+
+import glob
+import os
+
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
-
-import requests # Not required
-from io import BytesIO # Not required
 
 # this makes our figures bigger
 pylab.rcParams['figure.figsize'] = 20, 12
 
+import sys
+sys.path.append('/home/kirepreshanth/Documents/Dissertation/generative-inpainting-pytorch') # TODO allow this to be passed as part of the args.
 
 from PIL import Image
 import numpy as np
 
 from maskrcnn_benchmark.config import cfg
-from GMaskRCNN import GMaskRCNN
 from predictor import COCODemo
-
-config_file = "/home/kirepreshanth/Documents/Dissertation/config.yaml"
-
-# This updates the config options with the same 
-# config file used to train the MaskRCNN
-cfg.merge_from_file(config_file)
-
-# model = GMaskRCNN(cfg)
-# test = model.getTest()
-# print(test)
-
-coco_demo = COCODemo(
-    cfg,
-    min_image_size=800,
-    confidence_threshold=0.7,
-)
+from generator import generateInpaintedImage
 
 
+# TODO Move these to a uitls file.
 def load(fileName):
     """
     Given an url of an image, downloads the image and
@@ -43,13 +32,72 @@ def load(fileName):
     return image
 
 def imshow(img):
+    plt.figure()
     plt.imshow(img[:, :, [2, 1, 0]])
     plt.axis("off")
     plt.show()
     
+def creatDirectoryIfNotExists(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
-image = load("test_image_2_full.png") #TODO need to test an image from the test set. NOT Training set
-#imshow(image)
+parser = ArgumentParser()
+parser.add_argument('--config', type=str, default='/home/kirepreshanth/Documents/Dissertation/generative-inpainting-pytorch/checkpoints/cityscapes/hole_benchmark/config.yaml',
+                    help="training configuration")
+parser.add_argument('--seed', type=int, help='manual seed')
+parser.add_argument('--checkpoint_path', type=str, default='/home/kirepreshanth/Documents/Dissertation/generative-inpainting-pytorch/checkpoints/cityscapes/hole_benchmark', help='Path to the checkpoints where the latest GAN model is saved.')
+parser.add_argument('--folder', type=str, default='/home/kirepreshanth/Documents/Dissertation/datasets/cityscapes/occluded_dataset/leftImg8bit_co/val', help='Folder containing dataset with occlusions.')
+parser.add_argument('--mask', type=str, default='/home/kirepreshanth/Documents/Dissertation/generative-inpainting-pytorch/examples/center_mask_256.png')
+parser.add_argument('--output', type=str, default='/home/kirepreshanth/Documents/Dissertation/generative-inpainting-pytorch/examples/output_1.png')
+parser.add_argument('--flow', type=str, default='')
+parser.add_argument('--iter', type=int, default=0)
 
-predictions = coco_demo.run_on_opencv_image(image)
-imshow(predictions)
+args = parser.parse_args()
+
+for filePath in glob.glob(args.folder + "/**/*.png"):
+    
+    pathParts = filePath.split('/')
+    
+    fileName = pathParts[-1]
+    city = pathParts[-2]
+        
+    name, ext = fileName.split('.')
+    
+    fileName_occluded_instanceSegmented = name + "_ois." + ext
+    fileName_filled_IS = name + "_fis." + ext
+
+    image_occluded = load(filePath)
+
+    inpainted_image = generateInpaintedImage(args, filePath)
+    inpainted_image = np.array(inpainted_image)[:, :, [2, 1, 0]]
+
+    config_file = "/home/kirepreshanth/Documents/Dissertation/config.yaml"
+
+    # This updates the config options with the same 
+    # config file used to train the MaskRCNN
+    cfg.merge_from_file(config_file)
+
+    coco_demo = COCODemo(
+        cfg,
+        min_image_size=800,
+        confidence_threshold=0.7,
+    )
+
+    predictions_occluded = coco_demo.run_on_opencv_image(image_occluded)
+    predictions_filled = coco_demo.run_on_opencv_image(inpainted_image)
+    
+    pathParts[-3] = pathParts[-3] + '_is_128'
+    segmented_folder = '/'.join(pathParts[:-2])
+    
+    creatDirectoryIfNotExists(segmented_folder)
+    city_folder = segmented_folder + '/' + city
+    creatDirectoryIfNotExists(city_folder)
+    
+    predictions_occluded = Image.fromarray(predictions_occluded[:, :, [2, 1, 0]])
+    predictions_filled = Image.fromarray(predictions_filled[:, :, [2, 1, 0]])
+    
+    predictions_occluded.save(city_folder + '/' + fileName_occluded_instanceSegmented)
+    predictions_filled.save(city_folder + '/' + fileName_filled_IS)
+    
+    #imshow(predictions_occluded)
+    #imshow(predictions_filled)
